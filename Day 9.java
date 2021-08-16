2fe477a880709b2ed4e414164436d2278954bf5d
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-08-16 22:59:14 +0530

    Remove Leaves In Generic Tree
    The function is expected to remove all leaves from the tree.

---
+++ Day 9.java
@@ -0,0 +1,18 @@
+public static void removeLeaves(Node node) {
+   
+    int count =0;
+    for(int i= node.children.size()-1;i>=0; i--)
+    {
+        Node child = node.children.get(i);
+        if( child.children.size() == 0)
+        {
+            node.children.remove(child);
+        }
+    }
+    for(Node child : node.children)
+    {
+        removeLeaves(child);
+    }
+
+    
+  }
