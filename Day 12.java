21f2579574ce9d500d2a4308c4c1bb12da4e7643
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-08-20 00:20:00 +0530

    Find In Generic Tree
    The function is expected to find the given data in the tree, if found it should return true or return false.

--- Day 12.java
+++ Day 12.java
@@ -1,1 +1,36 @@
-
+public static boolean find(Node node, int data) {
+    
+    boolean count = false;
+    
+    for(Node child : node.children)
+    {
+        count=find(child,data);
+        
+        if(count == true)
+        {
+            break;
+        }
+    }
+    if ( count == false)
+    {
+    for(Node child : node.children)
+    {
+        if(child.data == data)
+        {
+            count = true;
+        }
+        else{
+            count = false;
+        }
+    }
+    }
+    
+    if(count == true)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+  }
