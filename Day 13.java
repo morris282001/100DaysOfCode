4d6eafed7e728b8f006f0f5fa024406be21d70d7
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-08-22 16:47:46 +0530

    Lowest Common Ancestor (generic Tree)
    The function is expected to return+ the lowest common ancestor of two data values that are passed to it.

--- Day 13.java
+++ Day 13.java
@@ -1,1 +1,47 @@
-
+  public static int lca(Node node, int d1, int d2) {
+    // write your code here
+    
+    if(node.data == d1 && node.data == d2)
+    {
+        return node.data;
+    }
+    for(Node child : node.children)
+    {
+      boolean c1 = find(child,d1);
+      if(c1 == true)
+      {
+          
+          boolean c2= find(child,d2);
+          
+          if(c2 == true)
+          {
+              
+              int lca = lca(child,d1,d2);
+              return lca;
+          }
+          
+          
+      }
+    }
+    return node.data;
+    
+  }
+   
+    private static boolean find(Node node, int data)
+    {
+        if(node.data == data)
+        {
+           return true;
+        }
+        for(Node child : node.children)
+        {
+            boolean f = find(child,data);
+            
+            if(f == true)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
