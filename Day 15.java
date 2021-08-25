420271f60cc487f64a1c06b02db819710c0c82ca
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-08-26 00:08:48 +0530

    Are Trees Similar In Shape
    The function is expected to check if the two trees passed to it are similar in shape or not.

--- Day 15.java
+++ Day 15.java
@@ -1,1 +1,21 @@
+public static boolean areSimilar(Node n1, Node n2) {
+// write your code here
+boolean count = false;
+if(n1.children.size() != n2.children.size())
+{
+return false;
+}
+else
+{
+for(int i=0 ;i< n1.children.size();i++)
+{
+count = areSimilar(n1.children.get(i),n2.children.get(i));
+if(count == false)
+{
+return false;
+}
+}
+}
+return true;
 
+}
