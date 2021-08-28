89c29821ff5fcd91ea7b0cbe134de5adb122624c
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-08-28 21:16:18 +0530

    Node To Root Path In Generic Tree
    The function is expected to return in form of linked list the path from element to root, if the element with data is found.

---
+++ Day 18.java
@@ -0,0 +1,15 @@
+
+  public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
+    if (node.data == data) {
+      ArrayList<Integer> path = new ArrayList<>();
+      path.add(node.data);
+      return path;
+    }
+
+    for (Node child : node.children) {
+      ArrayList<Integer> ptc = nodeToRootPath(child, data);
+      if (ptc.size() > 0) {
+        ptc.add(node.data);
+        return ptc;
+      }
+    }
