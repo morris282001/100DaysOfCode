00cccb33271cfd3b0d0641e503de725171f3f1ea
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-03 23:56:05 +0530

    Iterative Pre, Post And Inorder Traversals Of Binary Tree
    he function is expected to print pre order, in order and post order of the tree in separate lines (first pre, then in and finally post order).

--- Day 22.java
+++ Day 22.java
@@ -1,1 +1,33 @@
-
+public static void iterativePrePostInTraversal(Node node) {
+   
+    Stack<Pair> s = new Stack<>();
+    Pair p = new Pair(node,1);
+    s.push(p);
+    String pre = "";
+    String in = "";
+    String post = "";
+    
+    while(!s.empty())
+    {
+        Pair top = s.peek();
+        if(top.state == 1)
+        {
+            pre = pre + node.data +" ";
+         top.state++;
+         Pair pr = new Pair(node.left,1);
+         s.push(pr);
+        }
+        else if(top.state ==2)
+        {
+           in = in + node.data +" "; 
+           top.state++;
+           Pair pr = new Pair(node.right,1);
+           s.push(pr);
+           
+        }
+        else{
+            post = post + node.data+" ";
+            s.pop();
+        }
+    }
+  }
