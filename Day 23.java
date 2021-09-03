f6ebd4733d9f5a818a5c8053bedbf8ffd3895081
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-04 00:06:35 +0530

    Find And Nodetorootpath In Binary Tree
    The functions are expected to 
        3.1. find -> return true or false depending on if the data is found in binary tree.
        3.2. nodeToRoot -> returns the path from node (correspoding to data) to root in 
        form of an arraylist (root being the last element)

--- Day 23.java
+++ Day 23.java
@@ -1,1 +1,55 @@
+public static boolean find(Node node, int data)
+{
 
+if(node == null)
+{
+return false;
+}
+else if(node.data == data)
+{
+return true;
+}
+boolean c1 = find(node.left,data);
+boolean c2 = find(node.right,data);
+if(c1 == true)
+{
+return true;
+}
+else{
+return c2;
+}
+
+}
+
+public static ArrayList nodeToRootPath(Node node, int data){
+
+if(node == null)
+{
+return new ArrayList<>();
+}
+else if(node.data == data)
+{
+ArrayList array = new ArrayList<>();
+array.add(node.data);
+return array;
+}
+ArrayList a1 = new ArrayList<>();
+ArrayList a2 = new ArrayList<>();
+
+a1 = nodeToRootPath(node.left,data);
+a2 = nodeToRootPath(node.right,data);
+
+if( a1.size() > 0)
+{
+a1.add(node.data);
+return a1;
+}
+else if(a2.size()>0)
+{
+a2.add(node.data);
+return a2;
+}
+return new ArrayList<>();
+
+
+}
