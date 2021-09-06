c0b609c4607eb2853f70dffe76b7deba4a900e7f
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-06 13:31:31 +0530

    Print K Levels Down
    The function is expected to print in different lines all nodes which are k level deep.

--- Day 24.java
+++ Day 24.java
@@ -1,1 +1,41 @@
-
+public static void printKLevelsDown(Node node, int k){
+    // write your code here
+    Queue<Node> q = new ArrayDeque<>();
+    q.add(node);
+    if( k == 0)
+    {
+        System.out.println(node.data);
+    }
+ 
+    
+    
+    else{
+       while(k>0)
+       {
+           Queue<Node> qc = new ArrayDeque<>();
+           
+           
+           while( q.size() > 0)
+           {
+               node = q.element();
+               if(node.left != null)
+               {
+                   qc.add(node.left);
+               }
+               if(node.right != null)
+               {
+                   qc.add(node.right);
+               }
+               q.remove();
+           }
+         
+          q = qc;
+          k--;
+       }
+       while( q.size() > 0)
+       {
+           node = q.remove();
+           System.out.println(node.data);
+       }
+  }
+  }
