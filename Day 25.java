a1ccbb682daee84dde2a2b5be900e0a309628fb4
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-08 20:12:00 +0530

    Print Nodes K Distance Away
    The function is expected to print all nodes which are k distance away in any direction from node with value equal to data.

--- Day 25.java
+++ Day 25.java
@@ -1,1 +1,53 @@
+public static void printKNodesFar(Node node, int data, int k) {
 
+path = new ArrayList<>();
+find(node,data);
+for(int i=0;i {
+printKlevelsDown(path.get(i),k-i,i==0?null : path.get(i-1));
+}
+}
+    
+    
+static ArrayList path;
+public static boolean find(Node node,int data)
+{
+if(node == null)
+{
+return false;
+}
+
+if(node.data == data)
+{
+path.add(node);
+return true;
+}
+boolean fl = find(node.left,data);
+if(fl)
+{
+path.add(node);
+return true;
+}
+boolean fr = find(node.right,data);
+if(fr)
+{
+path.add(node);
+return true;
+}
+return false;
+}
+    
+    
+
+public static void printKlevelsDown(Node node, int k, Node blocker)
+{
+if(node == null || k<0 || node == blocker)
+{
+return;
+}
+if(k == 0)
+{
+System.out.println(node.data);
+}
+printKlevelsDown(node.left, k-1, blocker);
+printKlevelsDown(node.right, k-1, blocker);
+}
