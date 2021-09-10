e5815901d4082f3b5d630efd76b3457993367c2b
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-10 19:09:30 +0530

    Path To Leaf From Root In Range
     The function is expected to print all paths from root to leaves which have sum of nodes in range from lo to hi (both inclusive)

--- Day 26.java
+++ Day 26.java
@@ -1,1 +1,22 @@
-
+public static void pathToLeafFromRoot(Node node, String path, int sum, int lo, int hi){
+   if(node == null)
+   {
+       return;
+   }
+  if(node.left == null && node.right == null)
+  {
+      path = path + node.data;
+      sum = sum + node.data;
+      if(sum >= lo && sum <= hi)
+      {
+          System.out.println(path);
+      }
+      return;
+      
+  }
+  
+  pathToLeafFromRoot(node.left,path+node.data+" ",sum+node.data,lo,hi);
+  pathToLeafFromRoot(node.right,path+node.data+" ",sum+node.data,lo,hi);
+   
+  
+  }
