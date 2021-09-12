ef4b542e1bfc2df4bb50b49d01b54009fb9646a8
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-12 10:59:44 +0530

    Transform To Normal From Left-cloned Tree
    The function is expected to convert a left-cloned tree back to it's original form.

--- Day 28.java
+++ Day 28.java
@@ -1,1 +1,17 @@
-
+public static Node transBackFromLeftClonedTree(Node node){
+    // write your code here
+    if(node == null || node.left == node)
+    {
+        return null;
+    }
+    
+   transBackFromLeftClonedTree(node.right);
+  
+    transBackFromLeftClonedTree(node.left.left);
+    node.left = node.left.left;
+    
+    return node;
+    
+   
+    
+  }
