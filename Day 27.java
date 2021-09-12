74102ff60dbc1c605018138358b0b9cfa994b0fb
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-12 10:58:33 +0530

    Transform To Left-cloned Tree
     The function is expected to create a new node for every node equal in value to it and inserted between itself and it's left child.

--- Day 27.java
+++ Day 27.java
@@ -1,1 +1,18 @@
-
+public static Node createLeftCloneTree(Node node){
+    // write your code here
+    if(node == null) 
+  {
+      return null ;
+  }
+  
+  createLeftCloneTree(node.left);
+  createLeftCloneTree(node.right);
+  
+ Node temp = new Node(node.data,node.left,null);
+   
+   node.left = temp;
+  return node;
+  
+  
+    
+  }
