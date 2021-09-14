f880ad14bcfbc169ca30d9b8d7e50c35fdf5dd0b
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-14 17:06:57 +0530

    Print Single Child Nodes
    The function is expected to print in separate lines, all such nodes which are only child of their parent.

--- Day 29.java
+++ Day 29.java
@@ -1,1 +1,22 @@
-
+public static void printSingleChildNodes(Node node, Node parent){
+   
+    if(node == null)
+    {
+        return;
+    }
+    if(node.left == null && node.right == null)
+    {
+        return;
+    }
+    if(node.left == null)
+    {
+        System.out.println(node.right.data);
+    }
+     if(node.right == null)
+    {
+        System.out.println(node.left.data);
+    }
+    printSingleChildNodes(node.left,node);
+    printSingleChildNodes(node.right,node);
+    
+  }
