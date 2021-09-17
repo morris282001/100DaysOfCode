896a8865a4592d8bd1a2f6dc874e728963ce6bd8
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-17 23:16:51 +0530

    Sum of a binary Tree

--- Day 31.java
+++ Day 31.java
@@ -1,1 +1,12 @@
-
+ public static int sum(Node node) 
+  {
+    if(node==null)
+    {
+        return 0;
+    }
+    int lsum = sum(node.left);
+    int rsum = sum(node.right);
+    int s = lsum + rsum + node.data;
+    
+    return s;
+  }
