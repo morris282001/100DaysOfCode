37a69df675daab86b06a69908a569b15a3d33fbb
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-17 23:21:05 +0530

    Maximum node in a binary tree

--- Day 32.java
+++ Day 32.java
@@ -1,1 +1,16 @@
 
+public static int max(Node node) {
+    
+    if(node == null)
+    {
+        return 0;
+        
+    }
+    int m1 = max(node.left);
+    int m2 = max(node.right);
+    int m3 = Math.max(m1,m2);
+    int m4= Math.max(m3,node.data);
+    return m4;
+  }
+
+  
