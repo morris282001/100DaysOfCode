62e531264d5df6065b036ddfabf86d51d83b126c
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-09-22 21:08:32 +0530

    Find a node in a bst

--- Day 34.java
+++ Day 34.java
@@ -1,1 +1,22 @@
+public static boolean find(Node node, int data){
+
+if(node == null)
+{
+return false;
+}
+else if(data < node.data)
+{
+return find(node.left, data);
+
+}
+else if(data > node.data)
+{
+return find(node.right, data);
+
+}
+else{
+return true;
+}
+
+}
 
