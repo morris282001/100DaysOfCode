aa4a1f7147f6bb910a55d6b8f2ea2bd314a80045
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-10-03 19:49:26 +0530

    Diameter of a binary tree
    The function is expected to return the number of edges between two nodes which are farthest from each other in terms of edges.

--- Day 35.java
+++ Day 35.java
@@ -1,1 +1,21 @@
+public static int diameter1(Node node) {
+// write your code here
+int d1= depth(node.left);
+int d2 = depth(node.right);
 
+return d1+d2+2;
+}
+
+public static int depth(Node node)
+{
+if(node == null)
+{
+return -1;
+}
+int h1 = depth(node.left);
+int h2 = depth(node.right);
+
+int h3 = Math.max(h1,h2);
+
+return h3+1;
+}
