cf05539dac673c58b8acf2f4d0e8fa94d5a0e1cc
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-10-05 00:08:56 +0530

    Tilt Of Binary Tree
    The function is expected to set the value of data member "tilt". "tilt" of a node is the absolute value of difference between sum of nodes in it's left subtree and right subtree. "tilt" of the whole tree is represented as the sum of "tilt"s of all it's nodes.

--- Day 36.java
+++ Day 36.java
@@ -1,1 +1,21 @@
+static int tilt = 0;
+public static int tilt(Node node){
+// write your code here to set the tilt data member
+if(node == null)
+{
+return 0;
+}
+int tl =tilt(node.left);
+int tr =tilt(node.right);
 
+int s1= sum(node.left);
+int s2= sum(node.right);
+
+int T = Math.abs(s1 -s2);
+
+tilt = T+tl+tr;
+
+return tilt;
+
+
+}
