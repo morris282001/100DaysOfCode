b2a8ad0a9e800622d0f01192efacb5d27047ea07
Morris Thakur <83053311+morris282001@users.noreply.github.com>
2021-10-06 21:58:49 +0530

    Add Node To Bst

--- Day 37.java
+++ Day 37.java
@@ -1,1 +1,26 @@
 
+public static Node add(Node node, int data) {
+// write your code here
+if(node == null)
+{
+Node temp = new Node(data,null,null);
+return temp;
+}
+else if(data < node.data)
+{
+Node t = add(node.left,data);
+node.left = t;
+return node;
+}
+else if( data > node.data)
+{
+Node t = add(node.right,data);
+node.right = t;
+return node;
+}
+else {
+return node;
+}
+
+
+}
